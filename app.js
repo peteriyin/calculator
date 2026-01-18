dccdf16bb933da1aba6c9d7b1810fe43b3403024
Peter <poderinde619@gmail.com>
2026-01-18 01:23:28 +0100

    output displays first/second operand and operator seperately

--- app.js
+++ app.js
@@ -26,24 +26,30 @@ function calculator() {
             return subtract(operand1, operand2);
         } else if (op == "/") {
             return divide(operand1, operand2);
+        } else if (op == "*") {
+            return multiply(operand1, operand2)
         };
-        return multiply(operand1, operand2);
     };
     let operator = null;
     let firstOperand = null;
     let secondOperand = null;
-    let isSecondOperand = false;
+    let isActive = false;
 
     clearButton.addEventListener("click", () => {
-        output.textContent = ""
-    })
+        output.textContent = (newDigit += "e");
+    });
 
     digits.forEach((digit) => {
         digit.addEventListener("click", (event) => {
-            if (!isSecondOperand) {
+            if (!isActive) {
                 firstOperand = Number(event.target.textContent);
                 output.append(firstOperand);
-            } else if (isSecondOperand) {
+            }
+            else if (isActive) {
+                if (secondOperand == null) {
+
+                    output.textContent = "";
+                }
                 secondOperand = Number(event.target.textContent);
                 output.append(secondOperand);
             };
@@ -52,7 +58,7 @@ function calculator() {
 
     operators.forEach((operate) => {
         operate.addEventListener("click", (event) => {
-            isSecondOperand = true;
+            isActive = true;
             operator = event.target.textContent;
             output.textContent = operator;
         });
@@ -60,7 +66,9 @@ function calculator() {
 
     equalTo.addEventListener("click", () => {
         const operation = operate(operator, firstOperand, secondOperand);
+        console.log(operation);
+
         output.textContent = (operation);
     });
-}
+};
 calculator();
